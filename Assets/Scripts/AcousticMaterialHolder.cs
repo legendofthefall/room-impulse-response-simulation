8c4ece55c30366f4c8843c57272fcf2d816b8407
kasa5320 <kashan.sardar@tu-lmenau.de>
2025-02-08 06:54:46 +0100

    RIR generation for furnished room in a separate scene

--- Assets/Scripts/AcousticMaterialHolder.cs
+++ Assets/Scripts/AcousticMaterialHolder.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class AcousticMaterialHolder : MonoBehaviour
 {
-    public AcousticMaterial acousticMaterial; // Reference to the AcousticMaterial Scriptable Object
+    public AcousticMaterial acousticMaterial;
 }
